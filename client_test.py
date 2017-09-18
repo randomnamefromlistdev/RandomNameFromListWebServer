cb8f9482ae072a7cc459a713045c7dddc337ee93
The Dude <thedude@somewhere.com>
2017-09-18 19:10:44 +0000

    Fixed client integration test to POST data and parse response.

--- client_test.py
+++ client_test.py
@@ -11,12 +11,12 @@ class TestClient(unittest.TestCase):
 
     def testGet(self):
         url = os.environ['TEST_URL']
+        print "URL: " + url
         test_text = '{"names":["Thomas, John P.","Gregory, Lucas A.","Samuels, Karl","Coen, Peter","Cohen, Greg","Douglass, Cathy R.","Gifford, Charles C.","Bassar, Ahmed H.","Hanson-Gretel, Andrew M."]}\r\n'
         for i in range(0,20):
-            response = urllib2.urlopen(url).read()
-            print "Response: " + response
-            #response = RandomNameFromListServer.test_handler(test_text)        
-            self.assertRegexpMatches(response,"^\"[A-Za-z\-]+, [A-Za-z\-]+( \w\.){0,1}\"$")
+            response = urllib2.urlopen(url=url, data=test_text).read().splitlines()
+            name = response[-1]
+            self.assertRegexpMatches(name,"^\"[A-Za-z\-]+, [A-Za-z\-]+( \w\.){0,1}\"$")
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
